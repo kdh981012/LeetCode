8253b72664c6185e995bc92fa00b63813a860df3
Dahye Kim <99456423+kdh981012@users.noreply.github.com>
2024-03-08 20:19:16 -0500

    Create 242-Valid-Anagram.java

---
+++ Java/242-Valid-Anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        char sArray[] = s.toCharArray();
+        Arrays.sort(sArray);
+        
+        char tArray[] = t.toCharArray();
+        Arrays.sort(tArray);
+          
+        if (Arrays.equals(sArray, tArray)) {
+            return true;
+        }
+        
+        return false;
+    }
+}
