941ffe239b380f0ce123a9b19604102a307afc19
Dahye Kim <99456423+kdh981012@users.noreply.github.com>
2024-03-08 19:52:37 -0500

    Create 1470-Shuffle-the-Array.java

---
+++ Java/1470-Shuffle-the-Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+        int[] result = new int[nums.length];
+		
+        for(int i = 0; i < 2*n; i++){
+            if(i % 2 == 0){
+                result[i] = nums[i/2];
+            }
+            else{
+                result[i] = nums[n+i/2];
+            }
+        }
+        return result;
+        
+    }
+}
