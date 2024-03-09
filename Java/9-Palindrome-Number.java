fbb0873eab78679986d889d22f6defa6d9deb20d
Dahye Kim <99456423+kdh981012@users.noreply.github.com>
2024-03-08 20:12:15 -0500

    Create 9-Palindrome-Number.java

---
+++ Java/9-Palindrome-Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = "" + x;
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
